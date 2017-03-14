4bdbaf99d36d450494815eb23b9d317d83a97f74
Julio Del Valle <julio.delvalle@possible.com>
2017-03-14 16:54:25 -0600

    Updating gulpfile. Removing SASS and CSS functionality

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ Gulp Boilerplate
  */
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
-const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const useref = require('gulp-useref');
 const uglify = require('gulp-uglify');
@@ -17,7 +16,6 @@ const gulpIf = require('gulp-if');
 const cache = require('gulp-cache');
 const del = require('del');
 const runSequence = require('run-sequence');
-const cleanCSS = require('gulp-clean-css');
 const inject = require('gulp-inject');
 const config = require('./config.json');
 
@@ -25,13 +23,7 @@ const config = require('./config.json');
  * Inject javascript file references to index.html file
  */
 gulp.task('index', function () {
-    // var target = gulp.src('app/index.html');
-    // var sources = gulp.src(['./app/js/vendor/three.min.js/','./app/js/vendor/**/*.js'], {read: false});
-
-    // return target.pipe(inject(sources)).pipe(gulp.dest('app/'));
-
     gulp.src('./app/index.html')
-        // .pipe(inject(gulp.src('./app/**/*.js', {read: false}), {relative: true}))
         .pipe(inject(gulp.src(['./app/js/vendor/three.min.js/','./app/js/vendor/**/*.js'], {read: false}), {relative: true}))
         .pipe(gulp.dest('app/'));
 });
@@ -42,33 +34,17 @@ gulp.task('index', function () {
  */
 gulp.task('lint', function() {
     return gulp.src('./app/js/**/*.js')
-        .pipe(jshint())
+        .pipe(jshint({esversion: 6}))
         .pipe(jshint.reporter('default', { verbose: true }));
-});
-
-/**
- * Compile SASS into CSS
- */
-function compileSass(cfg) {
-    return gulp.src(cfg.sass.src)
-        .pipe(sass())
-        .pipe(gulp.dest(cfg.css.dest))
-        .pipe(browserSync.reload({stream: true}));
-}
-
-gulp.task('sass', function() {
-    compileSass(config.app);
 });
 
 /**
  * Watch project files and reload the screen
  */
 function watchAll(cfg){
-    gulp.watch(cfg.sass.src, ['sass']);
-    gulp.watch(cfg.js.src, ['lint']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch(cfg.html.src, browserSync.reload);
-    gulp.watch(cfg.js.src, browserSync.reload);  
+    gulp.watch(cfg.js.src, ['lint', browserSync.reload]);  
 }
 
 gulp.task('watch', function (){
@@ -79,7 +55,7 @@ gulp.task('watch', function (){
  * Default taks for the Development Environment
  */
 gulp.task('default', function (callback) {
-    runSequence(['index', 'sass', 'lint', 'browserSync', 'watch'],
+    runSequence(['index', 'lint', 'browserSync', 'watch'],
         callback
     );
 });
@@ -96,7 +72,7 @@ gulp.task('browserSync', function() {
 });
 
 /**
- * JS and CSS concatenation and minification
+ * JS concatenation and minification
 */
 gulp.task('useref', function(){
     userefUglify(config.app);
@@ -125,19 +101,6 @@ gulp.task('cache:clear', function (callback) {
 });
 
 /**
- * Minify CSS files
- */
-function minifyCss(cfg) {
-    return gulp.src(cfg.css.src)
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest(cfg.css.dest));
-}
-
-gulp.task('minify-css', function(){
-    minifyCss(config.app);
-});
-
-/**
  * Copy the images folder to the dist folder
  */
 function copyImagesFolder(cfg){
@@ -154,7 +117,7 @@ gulp.task('copy-images-folder', function(){
  */
 gulp.task('build', function (callback) {
     runSequence('clean:dist',
-        ['sass', 'minify-css', 'copy-images-folder', 'useref'],
+        ['copy-images-folder', 'useref'],
         callback
     )
 });
